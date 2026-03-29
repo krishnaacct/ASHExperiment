59f5db454ab6625304be733c4f3a3b202c343f7f
krishnaacct <krishna.acct@bkivv.org>
2026-03-29 21:40:18 +0530

    Delete components directory

--- components/common/ModuleLoader.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { WorkstationSkeleton } from './WorkstationSkeleton';
-
-const ModuleLoader: React.FC = () => {
-    // Standardize lazy-load fallback to match the common workstation layout
-    return <WorkstationSkeleton type="list-detail" />;
-};
-
-export default ModuleLoader;
